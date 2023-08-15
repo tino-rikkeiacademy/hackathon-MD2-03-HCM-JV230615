d64eedcac89c572b9dfc05ac9b770e395fd85365
quân <duongquan199999@gmail.com>
2023-08-15 14:19:52 +0700

    nopbai

---
+++ HCM_JS230615_AD_DuongMinhQuan/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Index from './component';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+function App() {
+  return (
+    <div >
+      <Index/>
+    </div>
+  );
+}
+
+export default App;
